b9bef2a97c87c3462ee5275708abdcae2e595fcd
AzureAD\NadunLiyanage <Nadun.Liyanage@theinsightsfamily.com>
2025-07-06 15:15:04 +0100

    Fetch Data from a Server Components

--- src/components/widget/widget.tsx
+++ src/components/widget/widget.tsx
@@ -1,7 +1,12 @@
 
 const Widget = async () => {
-  const delay = Math.floor(Math.random() * (4000 - 200 + 1)) + 200;
-  await new Promise(resolve => setTimeout(resolve, delay));
+  // Simulating a fetch request to get chart data
+  const b1 = Math.floor(Math.random() * (4000 - 200 + 1)) + 200;
+  const b2 = Math.floor(Math.random() * (4000 - 200 + 1)) + 200;
+  const b3 = Math.floor(Math.random() * (4000 - 200 + 1)) + 200;
+  const res = await fetch(`https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[${b1},${b2},${b3}]}]}}`, {
+    cache: 'no-store' // Disable caching for this request
+  });
   
   return (
     <div className="w-full rounded-xl border border-gray-200 p-6 shadow-sm bg-white space-y-4">
@@ -14,7 +19,10 @@ const Widget = async () => {
 
     {/* Content Block */}
     <div className="h-64 w-full bg-gray-50 border border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400 text-sm">
-        (Graph/Table Placeholder)
+        { 
+          res.ok ? ( <img src={res.url} alt="Chart" className="object-cover rounded-md" /> ) 
+            :  (<span>(Graph/Table Placeholder)</span>) 
+        }  
     </div>
     </div>
   )
